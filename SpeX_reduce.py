8fa4cdc7740a1fd05e70d2d4aea74d69f733c785
chase-urasaki <chasemu@hawaii.edu>
2024-12-12 14:15:26 -1000

    Added code to median flat and data

--- SpeX_reduce.py
+++ SpeX_reduce.py
@@ -1,20 +1,45 @@
+#%%
 # Import modules 
 import numpy as np 
 from astropy.io import fits
-
-#%% 
-# Pre-processing
-    # CAll routine to sort all files in a directory \
-def sort_files(directory):
-    
+import sys
+import glob
 
 
-# Call routine to get flats and median combine
-def get_flats():
-    for flat_frame in reduction.ini 
-    return 
-    #for file in directory open object header and search for 'Flat' 
+#%%
+def ds9(a):
+    """ ds9 shrotcut from Mike Bottom"""
+    fits.writeto('temp.fits', np.array(a), overwrite=True)
+    os.system('ds9 temp.fits -zoom to fit &')
+    return
+#%%
+# Combine flats by median
+def flats_combine(flats_directory): 
+    # Get all FITS files in the directory
+    file_list = glob.glob(os.path.join(flats_directory, "*.fits"))
+    if len(file_list) == 0:
+        raise ValueError("No FITS files found in the specified directory.")
 
+    # Read the data from all files into a list
+    flat_data = []
+    for file in file_list: 
+        with fits.open(file) as hdul:
+            data = hdul[0].data  # Assuming data is in the primary HDU
+            flat_data.append(data)
+
+    # Stack the arrays and compute the median along the third axis
+    flat_stack = np.stack(flat_data, axis=-1)  # Create a 3D array
+    median_flat = np.median(flat_stack, axis=-1)  # Median combine
+
+    return median_flat
+
+# Testing
+flats_dir = 'HW7/flats'
+median_flat = flats_combine(flats_directory=flats_dir)
+print("Median flat created with shape:", median_flat.shape)
+#%%
+ds9(median_flat)
+#%%
 # Call routine to get biases and median combine 
 def get_biases(): 
 
@@ -46,7 +71,18 @@ def flux_calibration():
 def get_traces():
 
 if __name__ == "__main__":
-    sort_files
+    if len(sys.argv) <7: 
+        print("Usage: flats_dir, bias_dir, darks_dir, wavelength_sol_dir, standards_dir, science_dir")
+    
+    flats_dir = str(sys.argv[1])
+    bias_dir = str(sys.argv[2])
+    darks_dir = str(sys.argv[3])
+    wavelength_sol_dir = str(sys.argv[4])
+    standards_dir = str(sys.argv[5])
+    science_dir = str(sys.argv[6])
+
+    flats_combine(flats_directory=flats_dir)
+
     flat_combine
     dark_combine
     bias_combine
--- sort_directory.py
+++ sort_directory.py
@@ -3,7 +3,17 @@ import os
 from astropy.io import fits 
 
 #%%
-def sort_directory():
+def make_folders():
     """ Function that sorts a directory of flats, biases, darks, wavelength solutions, and science frames"""
     # Create folders: 
-    
+    os.makedirs('flats')
+    os.makedirs('biases')
+    os.makedirs('darks')
+    os.makedirs('wavelength_cal')
+    os.makedirs('science')
+
+
+def sort_directory(directory):
+    for filename in os.listdir(directory): 
+        with fits.open(str(filename)) as hdul:
+            header = 
